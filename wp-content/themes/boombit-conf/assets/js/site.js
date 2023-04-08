23a73e488ca240c877c39ee114f70807dd136f62
Juan Espina <jmespina1997@gmail.com>
2023-04-07 21:17:55 -0400

    Update site.js

--- wp-content/themes/boombit-conf/assets/js/site.js
+++ wp-content/themes/boombit-conf/assets/js/site.js
@@ -1,6 +1,22 @@
-import 'bootstrap';
+import * as bootstrap from "bootstrap";
 
-jQuery( document ).ready( function( $ ) {
-
-  console.log('Loading')
+jQuery(document).ready(function ($) {
+  function BoombitTheme() {
+    const _self = this;
+    console.log("Loading");
+    _self.changeNavbarBgOnScroll();
+  }
+  BoombitTheme.prototype.changeNavbarBgOnScroll = function () {
+    $(document).scroll(function () {
+      const $nav = $(".navbar.fixed-top");
+      if ( $(this).scrollTop() > $nav.height()) {
+        $nav.addClass("navbar-light bg-light opacity-75");
+        $(".navbar-brand img").attr("src","https://boombit.agency/wp-content/uploads/2021/08/boombit-logo-dark.svg");
+      }else{
+        $nav.removeClass("navbar-white bg-white opacity-75");
+        $(".navbar-brand img").attr("src","https://boombit.agency/wp-content/uploads/2021/08/boombit-logo-light.svg");
+      }
+    });
+  };
+  new BoombitTheme();
 });
