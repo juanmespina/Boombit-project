c01b982e8a02651c996e74e53adcaf43fc6c436e
Juan Espina <jmespina1997@gmail.com>
2023-04-08 13:14:56 -0400

    Update site.js

--- wp-content/themes/boombit-conf/assets/js/site.js
+++ wp-content/themes/boombit-conf/assets/js/site.js
@@ -6,17 +6,26 @@ jQuery(document).ready(function ($) {
     console.log("Loading");
     _self.changeNavbarBgOnScroll();
   }
+  
   BoombitTheme.prototype.changeNavbarBgOnScroll = function () {
     $(document).scroll(function () {
       const $nav = $(".navbar.fixed-top");
-      if ( $(this).scrollTop() > $nav.height()) {
+      if ($(this).scrollTop() >= $nav.height()) {
         $nav.addClass("navbar-light bg-light opacity-75");
-        $(".navbar-brand img").attr("src","https://boombit.agency/wp-content/uploads/2021/08/boombit-logo-dark.svg");
-      }else{
-        $nav.removeClass("navbar-white bg-white opacity-75");
-        $(".navbar-brand img").attr("src","https://boombit.agency/wp-content/uploads/2021/08/boombit-logo-light.svg");
+        $(".navbar-brand img").attr(
+          "src",
+          "https://boombit.agency/wp-content/uploads/2021/08/boombit-logo-dark.svg"
+        );
+      } else {
+        $nav.removeClass("navbar-light bg-light opacity-75");
+        $(".navbar-brand img").attr(
+          "src",
+          "https://boombit.agency/wp-content/uploads/2021/08/boombit-logo-light.svg"
+        );
       }
     });
   };
+
+
   new BoombitTheme();
 });
