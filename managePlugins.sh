76567e4139305454db84ce69848464737391c2bf
Juan Espina <jmespina1997@gmail.com>
2023-04-09 23:16:07 -0400

    install plugins from sh

--- managePlugins.sh
+++ managePlugins.sh
@@ -1,8 +1,7 @@
          echo -e "1.0 ---- Installing plugins"
          echo -e "1.1 ---- Installing Local plugins"
-        wp plugin install plugins/advanced-custom-fields.zip --force --allow-root --activate
-
-
+         docker exec -i wpcli_boombit_conf sh -c "wp plugin install plugins/advanced-custom-fields-pro.zip --force --allow-root --activate"
+        
 		 echo -e "1.1 ---- Installing Online plugins"
-         wp plugin install contact-form-7 --activate
- 
+         docker exec -i wpcli_boombit_conf sh -c "wp plugin install contact-form-7 --activate"
+       
