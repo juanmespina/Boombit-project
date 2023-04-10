ff0192f063f7991e9e1a95fa0d0fbb97e35c04f1
Juan Espina <jmespina1997@gmail.com>
2023-04-09 22:13:05 -0400

    Fix missing change of classes in navbar

--- wp-content/themes/boombit-conf/assets/js/site.js
+++ wp-content/themes/boombit-conf/assets/js/site.js
@@ -4,21 +4,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function BoombitTheme() {
     const _self = this;
-    console.log("Loading");
     _self.changeNavbarBgOnScroll();
   }
   
   BoombitTheme.prototype.changeNavbarBgOnScroll = function () {
     const nav = document.querySelector(".navbar.fixed-top");
+    const links =  document.querySelector(".navbar .navbar-nav .nav-link");
     window.addEventListener('scroll', function() {
       if (window.pageYOffset >= nav.offsetHeight) {
         nav.classList.add("navbar-light", "bg-light", "opacity-75");
+        links.classList.remove('text-light');
+        links.classList.add('text-dark');
         document.querySelector(".navbar-brand img").setAttribute(
           "src",
           "https://boombit.agency/wp-content/uploads/2021/08/boombit-logo-dark.svg"
         );
       } else {
         nav.classList.remove("navbar-light", "bg-light", "opacity-75");
+        links.classList.remove('text-dark');
+        links.classList.add('text-light');
         document.querySelector(".navbar-brand img").setAttribute(
           "src",
           "https://boombit.agency/wp-content/uploads/2021/08/boombit-logo-light.svg"
